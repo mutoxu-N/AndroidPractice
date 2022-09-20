136fa84faef48616d17ab6526803c2528dbaf0f7
mutoxu=N <55544957+mutoxu-N@users.noreply.github.com>
2022-09-20 21:48:28 +0900

    lesson5-1

--- app/src/main/java/com/github/main/MainActivity.kt
+++ app/src/main/java/com/github/main/MainActivity.kt
@@ -2,10 +2,26 @@ package com.github.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ListView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val lvMenu = findViewById<ListView>(R.id.lvMenu)
+        lvMenu.onItemClickListener = ListItemClickListener()
+    }
+
+    private inner class ListItemClickListener : AdapterView.OnItemClickListener{
+        override fun onItemClick(parent: AdapterView<*>, view: View?, pos: Int, id: Long) {
+            val item = parent.getItemAtPosition(pos) as String
+            val show = "あなたが選んだ定食: $item"
+            Toast.makeText(this@MainActivity, show, Toast.LENGTH_LONG).show()
+        }
+
     }
 }
