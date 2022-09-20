a4650a2fe8ddb4a6fb1d1e2d8ba60ad0d41c5da9
mutoxu=N <55544957+mutoxu-N@users.noreply.github.com>
2022-09-21 08:21:01 +0900

    lesson5-2

--- app/src/main/java/com/github/main/MainActivity.kt
+++ app/src/main/java/com/github/main/MainActivity.kt
@@ -2,10 +2,35 @@ package com.github.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val lvMenu = findViewById<ListView>(R.id.lvMenu)
+        var menuList = mutableListOf<String>("から揚げ定食", "ハンバーグ定食", "生姜焼き定食",
+            "ステーキ定食", "野菜炒め定食", "とんかつ定食", "ミンチかつ定食", "チキンカツ定食",
+            "コロッケ定食", "回鍋肉定食", "麻婆豆腐定食", "青椒肉絲定食", "焼き魚定食", "焼肉定食"
+        )
+
+        val adapter = ArrayAdapter(this@MainActivity, android.R.layout.simple_list_item_1
+            , menuList)
+        lvMenu.adapter = adapter
+        lvMenu.onItemClickListener = ListItemClickListener()
+
     }
+
+    private inner class ListItemClickListener: AdapterView.OnItemClickListener {
+        override fun onItemClick(parent: AdapterView<*>?, view: View?, pos: Int, id: Long) {
+            val dialogFragment = OrderConfirmDialogFragment()
+            dialogFragment.show(supportFragmentManager, "OrderConfirmDialogFragment")
+        }
+
+    }
+
 }
