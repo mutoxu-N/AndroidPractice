107f6a2e5580d794991f6ab278585d51ff341e71
mutoxu=N <55544957+mutoxu-N@users.noreply.github.com>
2022-10-28 08:47:31 +0900

    lesson8

--- app/src/main/java/com/github/main/MainActivity.kt
+++ app/src/main/java/com/github/main/MainActivity.kt
@@ -1,11 +1,127 @@
 package com.github.main
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ContextMenu
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ListView
+import android.widget.SimpleAdapter
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    private var _menuList: MutableList<MutableMap<String, Any>> = mutableListOf()
+    private val _from = arrayOf("name", "price")
+    private val _to = intArrayOf(R.id.tvMenuNameRow, R.id.tvMenuPriceRow)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        _menuList = createTeishokuList()
+        val lvMenu = findViewById<ListView>(R.id.lvMenu)
+        val adapter = SimpleAdapter(this@MainActivity, _menuList, R.layout.row, _from, _to)
+        lvMenu.adapter = adapter
+        lvMenu.onItemClickListener = ListItemClickListener()
+        registerForContextMenu(lvMenu)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_options_menu_list, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        var returnVal = true
+        when(item.itemId) {
+            R.id.menuListOptionTeishoku ->
+                _menuList = createTeishokuList()
+            R.id.menuListOptionCurry ->
+                _menuList = createCurryList()
+            else ->
+                returnVal = super.onOptionsItemSelected(item)
+        }
+        val lvMenu = findViewById<ListView>(R.id.lvMenu)
+        val adapter = SimpleAdapter(this@MainActivity, _menuList, R.layout.row, _from, _to)
+        lvMenu.adapter = adapter
+        return returnVal
+    }
+
+    override fun onCreateContextMenu(menu: ContextMenu, view: View, menuInfo: ContextMenu.ContextMenuInfo) {
+        super.onCreateContextMenu(menu, view, menuInfo)
+
+        menuInflater.inflate(R.menu.menu_context_menu_list, menu)
+        menu.setHeaderTitle(R.string.menu_list_context_header)
+    }
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        var returnVal = true
+        val info = item.menuInfo as AdapterView.AdapterContextMenuInfo
+        val listPos = info.position
+        val menu = _menuList[listPos]
+
+        when(item.itemId) {
+            R.id.menuListContextDesc -> {
+                val desc = menu["desc"] as String
+                Toast.makeText(this@MainActivity, desc, Toast.LENGTH_LONG).show()
+            }
+
+            R.id.menuListContextOrder ->
+                order(menu)
+            else ->
+                returnVal = super.onContextItemSelected(item)
+        }
+        return returnVal
+    }
+
+    private fun order(menu: MutableMap<String, Any>) {
+        val menuName = menu["name"] as String
+        val menuPrice = menu["price"] as Int
+        val intent2MenuThanks = Intent(this@MainActivity, MenuThanksActivity::class.java)
+        intent2MenuThanks.putExtra("menuName", menuName)
+        intent2MenuThanks.putExtra("menuPrice", "${menuPrice}円")
+        startActivity(intent2MenuThanks)
+    }
+
+    private fun createCurryList(): MutableList<MutableMap<String, Any>> {
+        val menuList: MutableList<MutableMap<String, Any>> = mutableListOf()
+        menuList.add(mutableMapOf<String, Any>("name" to "ビーフカレー", "price" to 520, "desc" to "特選スパイスをきかせた国産ビーフ100%のカレーです。"))
+        menuList.add(mutableMapOf("name" to "ポークカレー", "price" to 420, "desc" to "特選スパイスをきかせた国産ポーク100%のカレーです。"))
+        menuList.add(mutableMapOf("name" to "ハンバーグカレー", "price" to 620, "desc" to "特選スパイスをきかせたカレーに手ごねハンバーグをトッピングです。"))
+        menuList.add(mutableMapOf("name" to "チーズカレー", "price" to 560, "desc" to "特選スパイスをきかせたカレーにとろけるチーズをトッピングです。"))
+        menuList.add(mutableMapOf("name" to "カツカレー", "price" to 760, "desc" to "特選スパイスをきかせたカレーに国産ロースカツをトッピングです。"))
+        menuList.add(mutableMapOf("name" to "ビーフカツカレー", "price" to 880, "desc" to "特選スパイスをきかせたカレーに国産ビーフカツをトッピングです。"))
+        menuList.add(mutableMapOf("name" to "からあげカレー", "price" to 540, "desc" to "特選スパイスをきかせたカレーに若鳥のから揚げをトッピングです。"))
+        return menuList
+    }
+
+    private fun createTeishokuList(): MutableList<MutableMap<String, Any>> {
+        val menuList: MutableList<MutableMap<String, Any>> = mutableListOf()
+        var menu = mutableMapOf<String, Any>("name" to "から揚げ弁当", "price" to 800, "desc" to "若鶏のから揚げにサラダ、ご飯とお味噌汁が付きます。")
+        menuList.add(menu)
+        menuList.add(mutableMapOf("name" to "ハンバーグ定食", "price" to 850, "desc" to "手ごねハンバーグにサラダ、ご飯とお味噌汁が付きます。"))
+        menuList.add(mutableMapOf("name" to "生姜焼き定食", "price" to 850, "desc" to "すりおろし生姜を使った生姜焼きにサラダ、ご飯とお味噌汁が付きます。"))
+        menuList.add(mutableMapOf("name" to "ステーキ定食", "price" to 1000, "desc" to "国産牛のステーキにサラダ、ご飯とお味噌汁が付きます。"))
+        menuList.add(mutableMapOf("name" to "野菜炒め定食", "price" to 750, "desc" to "季節の野菜炒めにサラダ、ご飯とお味噌汁が付きます。"))
+        menuList.add(mutableMapOf("name" to "とんかつ定食", "price" to 900, "desc" to "ロースとんかつにサラダ、ご飯とお味噌汁が付きます。"))
+        menuList.add(mutableMapOf("name" to "ミンチかつ定食", "price" to 850, "desc" to "手ごねミンチカツにサラダ、ご飯とお味噌汁が付きます。"))
+        menuList.add(mutableMapOf("name" to "チキンカツ定食", "price" to 900, "desc" to "ボリュームたっぷりチキンカツにサラダ、ご飯とお味噌汁が付きます。"))
+        menuList.add(mutableMapOf("name" to "コロッケ定食", "price" to 850, "desc" to "北海道ポテトコロッケにサラダ、ご飯とお味噌汁が付きます。"))
+        menuList.add(mutableMapOf("name" to "回鍋肉定食", "price" to 750, "desc" to "ピリ辛回鍋肉にサラダ、ご飯とお味噌汁が付きます。"))
+        menuList.add(mutableMapOf("name" to "麻婆豆腐定食", "price" to 800, "desc" to "本格四川風麻婆豆腐にサラダ、ご飯とお味噌汁が付きます。"))
+        menuList.add(mutableMapOf("name" to "青椒肉絲定食", "price" to 900, "desc" to "ピーマンの香り豊かな青椒肉絲にサラダ、ご飯とお味噌汁が付きます。"))
+        menuList.add(mutableMapOf("name" to "焼き魚定食", "price" to 850, "desc" to "鰆の塩焼きにサラダ、ご飯とお味噌汁が付きます。"))
+        menuList.add(mutableMapOf("name" to "焼肉定食", "price" to 950, "desc" to "特性たれの焼肉にサラダ、ご飯とお味噌汁が付きます。"))
+        return menuList
+    }
+
+    private inner class ListItemClickListener : AdapterView.OnItemClickListener {
+        override fun onItemClick(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+            val item = parent.getItemAtPosition(position) as MutableMap<String, Any>
+            order(item)
+        }
     }
 }
