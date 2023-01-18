b4ba9d7f54645198aae48d55cc91f77423a6ac5b
mutoxu=N <55544957+mutoxu-N@users.noreply.github.com>
2023-01-18 22:19:40 +0900

    lesson13

--- app/src/main/java/com/github/main/MainActivity.kt
+++ app/src/main/java/com/github/main/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.github.main
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fromNotification = intent.getBooleanExtra("fromNotification", false)
+        if(fromNotification) {
+            val btPlay = findViewById<Button>(R.id.btPlay)
+            val btStop = findViewById<Button>(R.id.btStop)
+            btPlay.isEnabled = false
+            btStop.isEnabled = true
+        }
+    }
+
+    fun onPlayButtonClick(view: View) {
+        val intent = Intent(this@MainActivity, SoundManageService::class.java)
+        startService(intent)
+
+        val btPlay = findViewById<Button>(R.id.btPlay)
+        val btStop = findViewById<Button>(R.id.btStop)
+        btPlay.isEnabled = false
+        btStop.isEnabled = true
+    }
+
+    fun onStopButtonClick(view: View) {
+        val intent = Intent(this@MainActivity, SoundManageService::class.java)
+        stopService(intent)
+
+        val btPlay = findViewById<Button>(R.id.btPlay)
+        val btStop = findViewById<Button>(R.id.btStop)
+        btPlay.isEnabled = true
+        btStop.isEnabled = false
     }
 }
