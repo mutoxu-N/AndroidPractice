d86fc367b8c19e7f26044376e533cc56f073cb89
mutoxu=N <55544957+mutoxu-N@users.noreply.github.com>
2023-02-09 16:04:49 +0900

    lesson15

--- app/src/main/java/com/github/main/MainActivity.kt
+++ app/src/main/java/com/github/main/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.github.main
 
+import android.content.Intent
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == 200 && resultCode == RESULT_OK) {
+            val bitmap = data?.getParcelableExtra<Bitmap>("data")
+            val ivCamera = findViewById<ImageView>(R.id.ivCamera)
+            ivCamera.setImageBitmap(bitmap)
+        }
+    }
+
+    fun onCameraImageClick(view: View) {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(intent, 200)
     }
 }
