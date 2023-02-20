2667bbc764a948a15ca6dbf43cee1cc66c3ba428
mutoxu=N <55544957+mutoxu-N@users.noreply.github.com>
2023-02-20 21:46:03 +0900

    lesson15

--- app/src/main/java/com/github/main/MainActivity.kt
+++ app/src/main/java/com/github/main/MainActivity.kt
@@ -1,14 +1,21 @@
 package com.github.main
 
+import android.annotation.SuppressLint
+import android.content.ContentValues
 import android.content.Intent
 import android.graphics.Bitmap
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.View
 import android.widget.ImageView
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    private var _imageUri: Uri? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -18,14 +25,24 @@ class MainActivity : AppCompatActivity() {
         super.onActivityResult(requestCode, resultCode, data)
 
         if(requestCode == 200 && resultCode == RESULT_OK) {
-            val bitmap = data?.getParcelableExtra<Bitmap>("data")
             val ivCamera = findViewById<ImageView>(R.id.ivCamera)
-            ivCamera.setImageBitmap(bitmap)
+            ivCamera.setImageURI(_imageUri)
         }
     }
 
     fun onCameraImageClick(view: View) {
+        val dateFormat = SimpleDateFormat("yyyyMMddHHmmss")
+        val now = Date()
+        val nowStr = dateFormat.format(now)
+        val fileName = "CameraIntentSamplePhoto_${nowStr}.jpg"
+
+        val values = ContentValues()
+        values.put(MediaStore.Images.Media.TITLE, fileName)
+        values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg")
+
+        _imageUri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
         val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, _imageUri)
         startActivityForResult(intent, 200)
     }
 }
